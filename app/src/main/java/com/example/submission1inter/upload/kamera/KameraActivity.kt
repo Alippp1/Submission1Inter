4218ab0fa6dcff996b39dd5e35a95cb88690528b
Alippp1 <aliftafn03@upi.edu>
2022-11-04 22:52:17 +0700

    Kamera Galeri Upload

--- app/src/main/java/com/example/submission1inter/upload/kamera/KameraActivity.kt
+++ app/src/main/java/com/example/submission1inter/upload/kamera/KameraActivity.kt
@@ -1,236 +1,124 @@
 package com.example.submission1inter.upload.kamera
 
-import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.submission1inter.R
 import com.example.submission1inter.databinding.ActivityKameraBinding
-import java.io.File
-
+import com.example.submission1inter.upload.UploadActivity
+import java.lang.Exception
+import java.nio.file.Files.createFile
 
 class KameraActivity : AppCompatActivity() {
-
-    private val filenameFormat = "dd-MMM-yyyy"
-    private lateinit var currentPhotoPath: String
     private lateinit var binding: ActivityKameraBinding
-    private val requiredPermissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
-    private val requestCodePermission = 100
-    private var getFile: File? = null
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == requestCodePermission) {
-            if (!allPermissionsGranted()) {
-                Toast.makeText(
-                    this,
-                    "Tidak mendapatkan Izin Akses Media",
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
-            } else {
-                Toast.makeText(
-                    this,
-                    "Akses media diizinkan",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    private fun allPermissionsGranted() = requiredPermissions.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityNewStoryBinding.inflate(layoutInflater)
+        binding = ActivityKameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        sessionManager = SessionManager(this@NewStoryActivity)
+        binding.captureImage.setOnClickListener { ambilFoto() }
+        binding.switchCamera.setOnClickListener {
+            cameraSelector = if (cameraSelector.equals(CameraSelector.DEFAULT_BACK_CAMERA)) CameraSelector.DEFAULT_FRONT_CAMERA
+            else CameraSelector.DEFAULT_BACK_CAMERA
+            mulaiKamera()
+        }
+    }
 
-        if (checkSelfPermission(requiredPermissions.toString()) == PackageManager.PERMISSION_GRANTED){
-            requestPermissions(requiredPermissions, requestCodePermission)
-        } else {
-            binding.btnCamera.setOnClickListener {
-                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                intent.resolveActivity(packageManager)
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+        mulaiKamera()
+    }
 
-                createCustomTempFile(application).also {
-                    val photoUri: Uri = FileProvider.getUriForFile(
-                        this@NewStoryActivity,
-                        "com.saifer.storyapp",
-                        it
+    private fun ambilFoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = createFile(application)
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Toast.makeText(
+                        this@KameraActivity,
+                        "Gagal mengambil Foto.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val intent = Intent()
+                    intent.putExtra("picture", photoFile)
+                    intent.putExtra(
+                        "isBackCamera",
+                        cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
                     )
-                    currentPhotoPath = it.absolutePath
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
-                    launcherIntentCamera.launch(intent)
+                    setResult(UploadActivity.CAMERA_X_RESULT, intent)
+                    finish()
                 }
             }
+        )
+    }
 
-            binding.btnGallery.setOnClickListener {
-                val intent = Intent()
-                intent.action = ACTION_GET_CONTENT
-                intent.type = "image/*"
-                val picker = Intent.createChooser(intent, "Pick a Picture")
-                launcherIntentGallery.launch(picker)
+    private fun mulaiKamera() {
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.view.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+
+            } catch (exc: Exception) {
+                Toast.makeText(
+                    this@KameraActivity,
+                    "Gagal memunculkan kamera.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
-
-            binding.btnAdd.setOnClickListener {
-                binding.progressBar.visibility = View.VISIBLE
-                postImage()
-                val i = Intent(this@NewStoryActivity, StoryActivity::class.java)
-                finish()
-                startActivity(i)
-            }
-
-        }
+        }, ContextCompat.getMainExecutor(this))
     }
 
-
-    private val launcherIntentCamera = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        if (it.resultCode == RESULT_OK) {
-            val myFile = File(currentPhotoPath)
-            getFile = myFile
-
-            val result = rotateBitmap(
-                BitmapFactory.decodeFile(myFile.path),
-                true
-            )
-
-            Glide.with(this@NewStoryActivity)
-                .load(result)
-                .apply( RequestOptions().override(1000,1000))
-                .into(binding.imgPreview)
-        }
-    }
-
-    private val launcherIntentGallery = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        if (result.resultCode == RESULT_OK) {
-            val selectedImg: Uri = result.data?.data as Uri
-
-            val myFile = uriToFile(selectedImg, this@NewStoryActivity)
-
-            getFile = myFile
-
-            binding.imgPreview.setImageURI(selectedImg)
-        }
-    }
-
-    private val timeStamp: String = SimpleDateFormat(
-        filenameFormat,
-        Locale.US
-    ).format(System.currentTimeMillis())
-
-    private fun createCustomTempFile(context: Context): File {
-        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(timeStamp, ".jpg", storageDir)
-    }
-
-    private fun rotateBitmap(bitmap: Bitmap, isBackCamera: Boolean = false): Bitmap {
-        val matrix = Matrix()
-        return if (isBackCamera) {
-            matrix.postRotate(90f)
-            Bitmap.createBitmap(
-                bitmap,
-                0,
-                0,
-                bitmap.width,
-                bitmap.height,
-                matrix,
-                true
-            )
+    private fun hideSystemUI() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
-            matrix.postRotate(-90f)
-            matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
-            Bitmap.createBitmap(
-                bitmap,
-                0,
-                0,
-                bitmap.width,
-                bitmap.height,
-                matrix,
-                true
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
-    }
-
-    private fun uriToFile(selectedImg: Uri, context: Context): File {
-        val contentResolver: ContentResolver = context.contentResolver
-        val myFile = createCustomTempFile(context)
-
-        val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
-        val outputStream: OutputStream = FileOutputStream(myFile)
-        val buf = ByteArray(1024)
-        var len: Int
-        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
-        outputStream.close()
-        inputStream.close()
-
-        return myFile
-    }
-
-    private fun postImage(){
-        if (getFile != null) {
-
-            val file = reduceFileImage(getFile as File)
-
-            val description = binding.edAddDescription.text.toString().toRequestBody("text/plain".toMediaType())
-            val requestImageFile = file.asRequestBody("image/jpg".toMediaTypeOrNull())
-            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
-                "photo",
-                file.name,
-                requestImageFile
-            )
-
-            val client = ApiConfig.getApiService().uploadImage(imageMultipart, description, "Bearer ${sessionManager.getToken()}")
-            client.enqueue(object : Callback<NewStoryResponse> {
-                override fun onResponse(
-                    call: Call<NewStoryResponse>,
-                    response: Response<NewStoryResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        val responseBody = response.body()
-                        if (responseBody != null && !responseBody.error!!) {
-                            Toast.makeText(this@NewStoryActivity, responseBody.message, Toast.LENGTH_SHORT).show()
-                            binding.progressBar.visibility = View.GONE
-                        }
-                    } else {
-                        Toast.makeText(this@NewStoryActivity, response.message(), Toast.LENGTH_SHORT).show()
-                        binding.progressBar.visibility = View.GONE
-                    }
-                }
-                override fun onFailure(call: Call<NewStoryResponse>, t: Throwable) {
-                    Toast.makeText(this@NewStoryActivity, "Upload Gagal", Toast.LENGTH_SHORT).show()
-                    binding.progressBar.visibility = View.GONE
-                }
-            })
-        } else {
-            binding.progressBar.visibility = View.GONE
-            Toast.makeText(this@NewStoryActivity, "Upload Gagal.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun reduceFileImage(file: File): File {
-        val bitmap = BitmapFactory.decodeFile(file.path)
-        var compressQuality = 100
-        var streamLength: Int
-        do {
-            val bmpStream = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
-            val bmpPicByteArray = bmpStream.toByteArray()
-            streamLength = bmpPicByteArray.size
-            compressQuality -= 5
-        } while (streamLength > 1000000)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
-        return file
+        supportActionBar?.hide()
     }
 }
-
